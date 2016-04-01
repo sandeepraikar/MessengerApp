d2b8240637f2d3a952f7a36b73e1d2a0d4ae16e6
sandeepraikar <snraikar@gmail.com>
2016-03-31 21:31:06 -0500

    Added a new Resource URL - /messages

--- src/main/java/edu/sraikar/messagengerapp/resources/MessageResource.java
+++ src/main/java/edu/sraikar/messagengerapp/resources/MessageResource.java
@@ -1,7 +1,15 @@
 package edu.sraikar.messagengerapp.resources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages")
 public class MessageResource {
 
+	@GET      // This is to specify the relevant HTTP method!!
+	@Produces(MediaType.TEXT_PLAIN) //This specifies response Format (TEXT_PLAIN indicates that the response is plain text)
 	public String getMessages(){
 		return "HelloWorld";
 	}
