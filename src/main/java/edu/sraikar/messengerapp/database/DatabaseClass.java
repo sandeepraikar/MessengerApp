04b9a239f583507d6c70cce5593540a366d82f7d
sandeepraikar <snraikar@gmail.com>
2016-04-03 11:17:17 -0500

    Added methods to fake back end

--- src/main/java/edu/sraikar/messengerapp/database/DatabaseClass.java
+++ src/main/java/edu/sraikar/messengerapp/database/DatabaseClass.java
@@ -11,11 +11,11 @@ public class DatabaseClass {
 	private static Map<Long,Message> messages = new HashMap<>();
 	private static Map<Long,Profile> profiles = new HashMap<>();
 	
-	public Map<Long,Message> getMessages(){
+	public static Map<Long,Message> getMessages(){
 		return messages;
 	}
 	
-	public Map<Long,Profile> getProfile(){
+	public static Map<Long,Profile> getProfile(){
 		return profiles;
 	}
 	
