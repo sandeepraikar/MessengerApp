08996e62ef1c1e9da764471d4bcb8bcd6299e2ae
sandeepraikar <snraikar@gmail.com>
2016-04-05 20:32:28 -0500

    Added implementation for POST method (addMessage)

--- src/main/java/edu/sraikar/messengerapp/resources/MessageResource.java
+++ src/main/java/edu/sraikar/messengerapp/resources/MessageResource.java
@@ -2,7 +2,9 @@ package edu.sraikar.messengerapp.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,5 +43,22 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long id){
 		return service.getMessage(id);
+	}
+	
+	//Creating a POST request
+	/*
+	 * Use @Consumes to specify the expected request body format
+	   basically Content-header type should have the correct format set for POST request
+		
+		example:
+		Header_type     header_value
+		Content-Type    application/json
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON) //This indicates that this post request consumes content of json format
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) //Here jersey is gonna automatically convert the json object passed to Message instance
+	{
+		return service.addMessage(message);
 	}
 }
