0f6f42ee2dbf405fd4acb25c0e31209fc153aaf1
sandeepraikar <snraikar@gmail.com>
2016-04-06 11:42:41 -0500

    Added UpdateMessage API

--- src/main/java/edu/sraikar/messengerapp/resources/MessageResource.java
+++ src/main/java/edu/sraikar/messengerapp/resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -61,4 +62,15 @@ public class MessageResource {
 	{
 		return service.addMessage(message);
 	}
+	
+	//Update operation
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message){
+		message.setId(id);
+		return service.udateMessage(message);
+	}
+	
 }
