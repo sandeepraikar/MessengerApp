b83868e8e3d93a6b9f6149810d5cee79977b3195
sandeepraikar <snraikar@gmail.com>
2016-04-09 10:40:07 -0500

    Added ContextParam annotation

--- src/main/java/edu/sraikar/messengerapp/resources/InjectDemoResource.java
+++ src/main/java/edu/sraikar/messengerapp/resources/InjectDemoResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -24,5 +27,20 @@ public class InjectDemoResource {
 		return "Matrix param: "+ matrixParam+" customHeader:  "+header + " Cookie: "+cookie ;
 	}
 	
+	//@ Context UriInfo is for accessing info abt uri, etc..
+	//@ Context HttpHeaders provides us the handle on all the headers which the resource is serving
+	/*
+	 * Exanple: from the above method getParamUsingAnnotations(args..)
+	 * The user need not know about the names of all the param attributes and can instead use HttpHeaders to extract
+	 * and process it
+	 * 
+	 */
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers){
+		String absolutePath= uriInfo.getAbsolutePath().toString();
+		String cookie = headers.getCookies().toString();
+		return "Path : "+ absolutePath +" Cookie : "+ cookie;
+	}
 
 }
