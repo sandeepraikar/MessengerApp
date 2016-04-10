9b99a04802793ad763e943d9046a2871e6fdf9ae
sandeepraikar <snraikar@gmail.com>
2016-04-10 12:07:58 -0500

    Updated CommentResource

--- src/main/java/edu/sraikar/messengerapp/resources/CommentResource.java
+++ src/main/java/edu/sraikar/messengerapp/resources/CommentResource.java
@@ -1,15 +1,48 @@
 package edu.sraikar.messengerapp.resources;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import edu.sraikar.messengerapp.model.Comment;
+import edu.sraikar.messengerapp.service.CommentService;
 
 @Path("/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class CommentResource {
 
+	private CommentService commentService = new CommentService();
+	
 	@GET
-	public String test(){
-		return "new sub resource";
+	public List<Comment> getAllComments(@PathParam("messageId") long messageId){
+		return commentService.getAllComments(messageId);
+	}
+	
+	@POST
+	public Comment addComment(@PathParam("messageId") long messageId, Comment comment){
+		return commentService.addComment(messageId, comment);
+	}
+	
+	@PUT
+	@Path("/{commentId}")
+	public Comment updateComment(@PathParam("messageId") long messageId,@PathParam("commentId") long id, Comment comment){
+		comment.setId(id);
+		return commentService.updateComment(messageId, comment);
+	}
+	
+	@DELETE
+	@Path("/{commentId}")
+	public void deleteComment(@PathParam("messagesId") long messageId, @PathParam("commentId") long id){
+		commentService.removeComment(messageId, id);
 	}
 	
 	@GET
